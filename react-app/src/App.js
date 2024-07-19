ce6a833fa4e8da775a962c623ef2044e6f02cd33
Lyainc <koore12@gmail.com>
2024-07-20 03:36:17 +0900

    comment fix

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,26 +11,26 @@ import saveAs from "file-saver";
 
 function App() {
   const [data, setData] = useState(null);
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [isCellLoading, setIsCellLoading] = useState(false);
   const divRef = useRef(null);
-  // const fetchData = async (controller) => {
-  //   try {
-  //     setLoading(true);
-  //     setError(null);
-  //     const response = await axios.get("http://127.0.0.1:8000/data", { signal: controller.signal });
-  //     setData(response.data);
-  //   } catch (e) {
-  //     if (axios.isCancel(e)) {
-  //       console.log('Request canceled', e.message);
-  //     } else {
-  //       setError(e);
-  //     }
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
+  const fetchData = async (controller) => {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await axios.get("http://127.0.0.1:8000/data", { signal: controller.signal });
+      setData(response.data);
+    } catch (e) {
+      if (axios.isCancel(e)) {
+        console.log('Request canceled', e.message);
+      } else {
+        setError(e);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const refreshData = async () => {
     setIsCellLoading(true);
@@ -57,35 +57,35 @@ function App() {
     })
   }
 
-  // useEffect(() => {
-  //   const controller = new AbortController();
-  //   fetchData(controller);
-  //   return () => {
-  //     controller.abort();
-  //   };
-  // }, []);
+  useEffect(() => {
+    const controller = new AbortController();
+    fetchData(controller);
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
-  // if (loading || !data) {
-  //   return (
-  //     <div>
-  //       <Box
-  //         sx={{
-  //           display: 'flex',
-  //           justifyContent: 'center',
-  //           alignItems: 'center',
-  //           height: '100vh'
-  //         }}
-  //       >
-  //         <CircularProgress />
-  //       </Box>
-  //     </div>
-  //   );
-  // }
+  if (loading || !data) {
+    return (
+      <div>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh'
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      </div>
+    );
+  }
   
-  // if (error)
-  //   return <div>Error...{error?.message}</div>;
+  if (error)
+    return <div>Error...{error?.message}</div>;
   
-  // const daysOfWeek = Object.keys(data.weekly_menu);
+  const daysOfWeek = Object.keys(data.weekly_menu);
 
   return (
     <div>
@@ -133,20 +133,18 @@ function App() {
         <Table ref={divRef}>
           <TableHead>
             <TableRow>
-              <TableCell></TableCell>
-              {/* {daysOfWeek.map(day => (
+              {daysOfWeek.map(day => (
                 <TableCell key={day} align='center'>{day}</TableCell>
-              ))} */}
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
             <TableRow>
-              <TableCell></TableCell>
-              {/* {daysOfWeek.map(day => (                
+              {daysOfWeek.map(day => (                
                 <TableCell key={day} align='center'>
                   <EditMenu data={data.weekly_menu[day]} />
                 </TableCell>
-              ))} */}
+              ))}
             </TableRow>
           </TableBody>
         </Table>
